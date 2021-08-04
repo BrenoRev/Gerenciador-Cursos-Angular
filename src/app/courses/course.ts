103131aa5995254a7401d4259889cc0d5ad1746b
Breno Rev <silvabreno462@gmail.com>
2021-08-03 23:59:48 -0300

    Criando componentes e construindo uma tabela

---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course{
+    id: number | undefined;
+    name: string | undefined;
+    imageUrl: string | undefined;
+    price: number | undefined;
+    code: string | undefined;
+    duration: number | undefined ;
+    rating: number | undefined;
+    releaseDate: string | undefined;
+}

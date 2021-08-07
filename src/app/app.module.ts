b161bdf37426e4357f7bebbe03fe1e24a3b9411f
Breno Rev <silvabreno462@gmail.com>
2021-08-07 11:31:06 -0300

    Adicionando rotas aos links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StartComponent } from './star/start.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error-404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StartComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+         // Rota vázia
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        // Rota Principal
+        path: 'courses', component: CourseListComponent
+      },
+      {
+          // Rota não encontrada
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
